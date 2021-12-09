b0223d65174bd1c6d8d88de65495d7174ba2f617
Gagan_Surya <gagansuryaleo@gmail.com>
2021-12-09 12:01:43 +0530

    spotify commit

---
+++ public/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Pages/HeaderComponent/Navbar';
+
+const App = () => {
+    return (
+      <section id="SpotifyMainBlock">
+        <article>
+          <Navbar/>
+        </article>
+      </section>
+    );
+}
+
+export default App
